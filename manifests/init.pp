edde452a4f3fd2a2347e4bc429d49e1a49c3e11e
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-06-03 12:17:00 +0000

    aggiunto rubygems

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,14 @@
 class librarian_puppet {
 
+  if $::operatingsystem == 'ubuntu' {
+    if !defined(Package['rubygems']) {
+      package{'rubygems':
+        ensure  => present,
+        before  => Package['librarian-puppet']
+      }
+    }
+  }
+
   package {'librarian-puppet':
     ensure    => installed,
     provider  => 'gem',
