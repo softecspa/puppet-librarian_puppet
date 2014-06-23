bee9b57c191e7c50cb474e3071f8f44b5699bbee
Fx <fpizzurro@nb-pizzurro.(none)>
2014-06-23 09:16:52 +0200

    modificata versione da installare

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,24 @@
 class librarian_puppet {
 
-  if $::operatingsystem == 'ubuntu' {
-    if !defined(Package['rubygems']) {
-      package{'rubygems':
-        ensure  => present,
-        before  => Package['librarian-puppet']
+  case $::operatingsystem {
+    'ubuntu': {
+      if !defined(Package['rubygems']) {
+        package{'rubygems':
+          ensure  => present,
+          before  => Package['librarian-puppet']
+        }
+      }
+
+      package {'librarian-puppet':
+        ensure    => '1.0.1',
+        provider  => 'gem',
+      }
+    }
+    'windows': {
+      exec {'install librarian puppet':
+        command => 'gem install librarian-puppet -v 1.0.1 --no-ri --no-rdoc'
+
       }
     }
   }
-
-  package {'librarian-puppet':
-    ensure    => installed,
-    provider  => 'gem',
-  }
-
 }
